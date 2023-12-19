e0e5bc4551a477f8757a2c6488e846d36c89a45b
xiaoxu03 <chao-xu21@tsinghua.edu.cn>
2023-12-20 00:36:54 +0800

    基本完成了游戏详情界面，TODO: 移植到比赛详情界面

--- app/src/components/Game.jsx
+++ app/src/components/Game.jsx
@@ -1,7 +1,7 @@
 import { useParams, useNavigate, Routes, Route, Link } from "@solidjs/router"
 import { onMount, createSignal, Switch, Match, For } from "solid-js";
 import { apiUrl } from "../utils";
-import { Flex, Heading, Image, Center, HStack, Button, VStack, Box, Table, Thead, Th, Tr, Td, Tbody } from "@hope-ui/solid";
+import { Flex, Heading, Image, Center, HStack, Button, VStack, Box, Table, Thead, Th, Tr, Td, Tbody, Spacer, Tag } from "@hope-ui/solid";
 import { SolidMarkdown } from "solid-markdown";
 import { Pagination } from "@ark-ui/solid";
 
@@ -211,13 +211,296 @@ function Ranklist() {
 }
 
 function Matches() {
+    const params = useParams(); // params.id
+
+    const [matchList, setMatchList] = createSignal();
+    const [currentPage, setCurrentPage] = createSignal(1);
+    const currentPageData = () => matchList().data;
+
+    function updateMatchList() {
+        fetch(
+            `${apiUrl}/games/${params.id}/matches?limit=${5}&offset=${5 * (currentPage() - 1)}`,
+            {
+                "method": "GET",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
+                },
+            }
+        )
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error(response.statusText);
+                }
+            })
+            .then((data) => {
+                setMatchList(data);
+                console.log(data);
+            });
+    }
+
+    onMount(() => {
+        updateMatchList();
+    });
+
+    const handlePageChange = (details) => {
+        setCurrentPage(details.page);
+        console.log(details.page);
+        updateMatchList();
+    }
+
+    const navigate = useNavigate();
+
+    const navigateToUser = (username) => {
+        navigate('/users/' + username);
+    }
+
     return (
-        <div>对局列表</div>
-    );
+        <>
+            {matchList() ? (
+                <Center>
+                    <VStack>
+                        <Center>
+                            <Table highlightOnHover width={"1200px"}>
+                                <Thead>
+                                    <Th width={"100px"}>对局编号</Th>
+                                    <Th width={"300px"}>对局时间</Th>
+                                    <Th width={"100px"}>备注</Th>
+                                    <Th width={"300px"}>选手1</Th>
+                                    <Th width={"300px"}>选手2</Th>
+                                    <Th width={"100px"}>对局状态</Th>
+                                </Thead>
+                                <Tbody>
+                                    <For each={currentPageData()}>
+                                        {(match, index) =>
+                                            <Tr>
+                                                <Td>
+                                                    <Heading>{match.id}</Heading>
+                                                </Td>
+                                                <Td>
+                                                    <Heading>{match.time}</Heading>
+                                                </Td>
+                                                <Td>
+                                                    <Heading>{match.tag}</Heading>
+                                                </Td>
+                                                <Td width={"400px"}>
+                                                    <HStack>
+                                                        <Box as="img" onClick={() => navigateToUser(match.players[0].user.username)} src={match.players[0].user.avatar_url} style={`cursor: pointer;width: 30px; height: 30px; border-radius: 50%; object-fit: cover;`} />
+                                                        <Box ml="10px">
+                                                            <Heading>{match.players[0].user.nickname}</Heading>
+                                                            <Heading fontSize="12px" color="gray">AI编号: {match.players[0].ai.id}</Heading>
+                                                        </Box>
+                                                        <Spacer />
+                                                        <Center height={"48px"} width={"48px"} backgroundColor={match.players[0].score > match.players[1].score ? "green" : match.players[0].score < match.players[1].score ? "red" : "gray"}>
+                                                            <Heading color="white" >{match.players[0].score}</Heading>
+                                                        </Center>
+                                                    </HStack>
+                                                </Td>
+                                                <Td width={"400px"}>
+                                                    <HStack>
+                                                        <Box as="img" onClick={() => navigateToUser(match.players[1].user.username)} src={match.players[1].user.avatar_url} style={`cursor: pointer;width: 30px; height: 30px; border-radius: 50%; object-fit: cover;`} />
+                                                        <Box ml="10px">
+                                                            <Heading>{match.players[1].user.nickname}</Heading>
+                                                            <Heading fontSize="12px" color="gray">AI编号: {match.players[1].ai.id}</Heading>
+                                                        </Box>
+                                                        <Spacer />
+                                                        <Center height={"48px"} width={"48px"} backgroundColor={match.players[1].score > match.players[0].score ? "green" : match.players[1].score < match.players[0].score ? "red" : "gray"}>
+                                                            <Heading color="white" >{match.players[1].score}</Heading>
+                                                        </Center>
+                                                    </HStack>
+                                                </Td>
+                                                <Td>
+                                                    <Tag colorScheme={match.state == "finished" ? "success" : match.state == "pending" || match.state == "running" ? "warning" : "danger"}>{match.state}</Tag>
+                                                </Td>
+                                            </Tr>
+                                        }
+                                    </For>
+                                </Tbody>
+                            </Table>
+                        </Center>
+                        <Pagination.Root
+                            count={matchList().count}
+                            pageSize={5}
+                            siblingCount={2}
+                            page={currentPage()}
+                            onPageChange={(details) => handlePageChange(details)}
+                        >
+                            {(api) => (
+                                <>
+                                    <Pagination.PrevTrigger>{"<"}</Pagination.PrevTrigger>
+                                    <For each={api().pages}>
+                                        {(page, index) =>
+                                            page.type === 'page' ? (
+                                                <Pagination.Item {...page}>{page.value}</Pagination.Item>
+                                            ) : (
+                                                <> </>
+                                            )
+                                        }
+                                    </For>
+                                    <Pagination.NextTrigger>{">"}</Pagination.NextTrigger>
+                                </>
+                            )}
+                        </Pagination.Root>
+                    </VStack >
+                </Center >
+            ) : (<div>加载中...</div >)
+            }
+        </>
+    )
 }
 
 function Submissions() {
+    const params = useParams(); // params.id
+
+    const [submissionList, setSubmissionList] = createSignal();
+    const [currentPage, setCurrentPage] = createSignal(1);
+    const [myself, setMyself] = createSignal();
+
+    onMount(() => {
+        if (localStorage.getItem('jwt')) {
+            fetch(`${apiUrl}/user`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+                }
+            })
+                .then((response) => {
+                    if (response.status === 200) {
+                        return response.json();
+                    } else {
+                        throw new Error(response.statusText);
+                    }
+                })
+                .then((data) => {
+                    setMyself(data);
+                })
+        }
+
+        updateSubmissionList();
+    });
+
+    const updateSubmissionList = () => {
+        fetch(
+            `${apiUrl}/games/${params.id}/ais?limit=${5}&offset=${5 * (currentPage() - 1)}&username=${myself.username}`,
+            {
+                "method": "GET",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
+                },
+            }
+        )
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error(response.statusText);
+                }
+            })
+            .then((data) => {
+                setSubmissionList(data);
+            });
+    }
+
+    const downloadAI = (ai) => {
+        console.log(ai);
+        fetch(
+            `${apiUrl}/games/${params.id}/ais/${ai.id}/file`,
+            {
+                "method": "GET",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
+                },
+            }
+        )
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.blob();
+                } else {
+                    throw new Error(response.statusText);
+                }
+            })
+            .then((data) => {
+                const blob = new Blob([data], { type: 'application/zip' });
+                const url = URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = `${ai.id}.zip`;
+                link.click();
+            });
+    }
+
+    const handlePageChange = (details) => {
+        setCurrentPage(details.page);
+        updateSubmissionList();
+    }
+
     return (
-        <div>提交列表</div>
+        <>
+            {submissionList() ? (
+                <Center>
+                    <VStack>
+                        <Center>
+                            <Table highlightOnHover width={"1000px"}>
+                                <Thead>
+                                    <Th>提交序号</Th>
+                                    <Th>提交时间</Th>
+                                    <Th>语言</Th>
+                                    <Th>提交状态</Th>
+                                    <Th>下载</Th>
+                                </Thead>
+                                <Tbody>
+                                    <For each={submissionList().data}>
+                                        {(submission, index) =>
+                                            <Tr>
+                                                <Td>{submission.id}</Td>
+                                                <Td>{submission.time}</Td>
+                                                <Td>{submission.sdk.name}</Td>
+                                                <Td>
+                                                    <Tag colorScheme={submission.status.state == "finished" ? "success" : submission.status.state == "pending" || submission.status.state == "running" ? "warning" : "danger"}>
+                                                        {submission.status.state}
+                                                    </Tag>
+                                                </Td>
+                                                <Td>
+                                                    <Button onClick={() => downloadAI(submission)}>下载</Button>
+                                                </Td>
+                                            </Tr>
+                                        }
+                                    </For>
+                                </Tbody>
+                            </Table>
+                        </Center>
+                        <Pagination.Root
+                            count={submissionList().count}
+                            pageSize={5}
+                            siblingCount={2}
+                            page={currentPage()}
+                            onPageChange={(details) => handlePageChange(details)}
+                        >
+                            {(api) => (
+                                <>
+                                    <Pagination.PrevTrigger>{"<"}</Pagination.PrevTrigger>
+                                    <For each={api().pages}>
+                                        {(page, index) =>
+                                            page.type === 'page' ? (
+                                                <Pagination.Item {...page}>{page.value}</Pagination.Item>
+                                            ) : (
+                                                <> </>
+                                            )
+                                        }
+                                    </For>
+                                    <Pagination.NextTrigger>{">"}</Pagination.NextTrigger>
+                                </>
+                            )}
+                        </Pagination.Root>
+                    </VStack >
+                </Center>
+            ) : (<div>加载中...</div >)
+            }
+        </>
     );
 }
